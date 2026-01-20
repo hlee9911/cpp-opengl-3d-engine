652019ba4f4fe982631d218cf3fce55b6b93bfe0
Hon <hlee0211@gmail.com>
2026-01-19 20:46:41 -0500

    Added Vertex Buffer and Uniforms

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,41 @@
 #include <vector>
 #include <string>
 
+struct Vec2
+{
+	float x = 0.0f;
+	float y = 0.0f;
+};
+
+Vec2 offset;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS)
+	{
+		switch (key)
+		{
+			case GLFW_KEY_UP:
+				offset.y += 0.01f;
+				break;
+			case GLFW_KEY_DOWN:
+				offset.y -= 0.01f;
+				break;
+			case GLFW_KEY_LEFT:
+				offset.x -= 0.01f;
+				break;
+			case GLFW_KEY_RIGHT:
+				offset.x += 0.01f;
+				break;
+			case GLFW_KEY_ESCAPE:
+				glfwSetWindowShouldClose(window, true);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
     if (!glfwInit())
@@ -17,7 +52,7 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopmentEngine", nullptr, nullptr);
 
     if (window == nullptr)
     {
@@ -25,6 +60,8 @@ int main(int argc, char* argv[])
 		glfwTerminate();
 		return -1;
     }
+
+	glfwSetKeyCallback(window, keyCallback);
 
     glfwSetWindowPos(window, 640, 360);
 	glfwMakeContextCurrent(window);
@@ -41,9 +78,16 @@ int main(int argc, char* argv[])
     std::string vertexShaderSource = R"(
         #version 330 core
         layout(location = 0) in vec3 position;
+		layout(location = 1) in vec3 color;
+
+		uniform vec2 uOffset;
+
+		out vec3 vColor;
+
         void main()
         {
-            gl_Position = vec4(position.x, position.y, position.z, 1.0);
+			vColor = color;
+            gl_Position = vec4(position.x + uOffset.x, position.y + uOffset.y, position.z, 1.0);
         }
     )";
 
@@ -75,10 +119,13 @@ int main(int argc, char* argv[])
     std::string fragmentShaderSource = R"(
         #version 330 core
         out vec4 FragColor;
+
+		in vec3 vColor;
+		uniform vec4 uColor;
         
         void main()
         {
-            FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            FragColor = vec4(vColor, 1.0) * uColor;
         }
     )";
 
@@ -102,6 +149,8 @@ int main(int argc, char* argv[])
 
 	//////////////////////////////////////////////
 
+	///////////// Shader Program //////////////
+
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
@@ -120,16 +169,72 @@ int main(int argc, char* argv[])
 
     std::vector<float> vertices =
     {
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f
     };
 
+	std::vector<unsigned int> indicies =
+	{
+		0, 1, 2,
+		0, 2, 3
+	};
+
+	/////////////////////////////////////////////
+
+	// Create Vertex Buffer Object (VBO)
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	// Bind the VBO
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// now convert the buffer data from cpu to gpu
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+	// unbind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Create Element Buffer Object (EBO)
+	GLuint ebo;
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicies.size() * sizeof(unsigned int), indicies.data(), GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// Create Vertex Array Object (VAO)
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// bind the EBO to the VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+	// specify the layout of the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// unbind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// set uniform locations
+	GLint uColorLoc = glGetUniformLocation(shaderProgram, "uColor");
+	GLint uOffsetLoc = glGetUniformLocation(shaderProgram, "uOffset");
+	
     while (!glfwWindowShouldClose(window))
     {
         // Cleara frame buffer
-        glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // green background
+        glClearColor(1.0f, 1.0f, 0.0f, 1.0f); // green background
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glUniform4f(uColorLoc, 0.0f, 1.0f, 0.0f, 1.0f); // set uniform color to green
+		glUniform2f(uOffsetLoc, offset.x, offset.y);
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
